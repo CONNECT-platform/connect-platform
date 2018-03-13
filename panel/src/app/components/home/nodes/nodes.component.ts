369727a98ff744233eb1d7002d21e906fc061340
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-03-13 22:29:53 +0100

    added search to nodes list in panel

--- panel/src/app/components/home/nodes/nodes.component.ts
+++ panel/src/app/components/home/nodes/nodes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, Renderer } from '@angular/core';
 
 import { BackendService } from '../../../services/backend.service';
 import { RegistryService } from '../../../services/registry.service';
@@ -11,7 +11,7 @@ import { RegistryService } from '../../../services/registry.service';
 })
 export class NodesComponent implements OnInit, OnDestroy {
 
-  nodes : Array<{
+  private _nodes : Array<{
     path: string;
     id: string;
     public?: boolean;
@@ -19,14 +19,19 @@ export class NodesComponent implements OnInit, OnDestroy {
 
   private _updateInterval;
 
+  searching : boolean = false;
+
+  @ViewChild('searchinput') searchInput : ElementRef;
+
   constructor(
+    private renderer : Renderer,
     private backend : BackendService,
     private registry : RegistryService,
   ) { }
 
   ngOnInit() {
     this.backend.nodes.subscribe(data => {
-      this.nodes = Object.entries(data.nodes).map(entry => {
+      this._nodes = Object.entries(data.nodes).map(entry => {
           return {
             path: entry[0],
             id: entry[1],
@@ -42,8 +47,27 @@ export class NodesComponent implements OnInit, OnDestroy {
     clearInterval(this._updateInterval);
   }
 
+  public get nodes() {
+    return this._nodes.filter(n => n.path.indexOf(this.searchInput.nativeElement.value) != -1);
+  }
+
+  public toggleSearch() {
+    if (this.searching) {
+      if (!this.searchInput.nativeElement.value) {
+        this.searching = false;
+      }
+    }
+    else {
+      this.searching = true;
+      setTimeout(() => {
+        this.renderer.invokeElementMethod(
+          this.searchInput.nativeElement, 'focus', []);
+      }, 10);
+    }
+  }
+
   private _update() {
-    for (let node of this.nodes) {
+    for (let node of this._nodes) {
       let signature = this.registry.signature(node.path);
       node.public = signature.public;
       node.method = signature.method;
