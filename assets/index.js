d30e5408e71d74145465903ee72440c21cbd2981
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-09-08 23:37:09 +0200

    the basic structure for the new design

--- assets/index.js
+++ assets/index.js
@@ -1,47 +1,66 @@
-const $win = $(window);
+var $holder;
 
-let jc = false;
-const correct = function() {
-  if (jc) return;
-  jc = true;
-  setTimeout(() => jc=false, 200);
+const prepare = function() {
+  $holder = $('.holder');
 
-  const margin_default = $win.height() * .5;
-  let $target = null;
-
-  $('.section').each((_, el) => {
+  $('div[animated]').each((_, el) => {
     const $el = $(el);
-
-    const top = $el.offset().top;
-    const height = $el.height();
-    const wintop = $win.scrollTop();
-    const winheight = $win.height();
-
-    let margin = margin_default;
-
-    if ($el.attr('switch-margin')) {
-      let factor = $el.attr('switch-margin');
-      margin = $win.height() * factor;
+    let index = 0;
+    let animations = [];
+    while (true) {
+      if ($el.attr(`p${index}`)) {
+        animations.push({
+          pos: $el.attr(`p${index}`) * 1,
+          x: $el.attr(`x${index}`) * 1,
+          y: $el.attr(`y${index}`) * 1,
+        });
+        index += 1;
+      }
+      else break;
     }
 
-    if (wintop > top - margin) {
-      $target = $el;
-    }
-
-    if (wintop > top) $el.addClass('active');
-    else $el.removeClass('active');
-
-    if (wintop > top + height || wintop + winheight < top) $el.removeClass('in');
-    else $el.addClass('in');
+    $el.data('animations', animations);
   });
 
-  if ($target && $target.is('.white')) $('body').addClass('white');
-  else $('body').removeClass('white');
+  $holder.scroll(animate);
+  animate();
 };
 
-$win.scroll(correct);
-$win.resize(correct);
+const animate = function() {
+  const pos = $holder.scrollTop()/$holder.height();
 
-setInterval(correct, 800);
+  $('img[backdrop]').css('transform', `rotate(${pos * 120}deg)`)
 
-$(document).ready(correct);
+  $('div[animated]').each((_, el) => {
+    const $el = $(el);
+    const animations = $el.data('animations');
+    const next = animations.filter(entry => entry.pos > pos);
+    const prev = animations.filter(entry => entry.pos <= pos);
+
+    let x = 0;
+    let y = 0;
+    if (next.length > 0 && prev.length > 0) {
+      let start = prev[prev.length - 1];
+      let end = next[0];
+
+      let r = (pos - start.pos)/(end.pos - start.pos);
+      x = r * (end.x - start.x) + start.x;
+      y = r * (end.y - start.y) + start.y;
+    }
+    else if (next.length > 0) {
+      let fixed = next[0];
+      x = fixed.x;
+      y = fixed.y;
+    }
+    else if (prev.length > 0) {
+      let fixed = prev[prev.length - 1];
+      x = fixed.x;
+      y = fixed.y;
+    }
+
+    $el.css('transform', `translateX(${x}vw) translateY(${y}vh)`);
+  })
+};
+
+$(window).resize(animate);
+$(document).ready(prepare);
