fb4af11e7f125f6e5bd09076ef57b682917d2c22
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2018-09-28 13:01:31 +0200

    fixed adder buttons, added console watcher to default recorder used for testing

--- platform/recorder/index.js
+++ platform/recorder/index.js
@@ -1,10 +1,16 @@
 const { JSONScenario } = require('./json-scenario');
+const { ScenarioEvents } = require('./scenario');
+const { ConsoleWatcher } = require('./console-watcher');
 const { Recorder } = require('./recorder');
 
 module.exports = (json, inputs, configs) => {
   return new Promise((resolve, reject) => {
-    let scenario = new JSONScenario(json, inputs, configs)
+    let cwatcher = new ConsoleWatcher();
+    let scenario = new JSONScenario(json, inputs, configs).subscribe(ScenarioEvents.prepared, () => {
+      scenario.watcher().mount('console', cwatcher.watch(console));
+    });
     let recorder = new Recorder().finished(() => {
+      cwatcher.unhook(console);
       resolve(recorder.recording);
     }).record(scenario);
   });
