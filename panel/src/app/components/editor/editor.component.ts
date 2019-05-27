eb0461c30ad4cfa41b01cccc1776afbfd009a249
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-05-27 16:05:28 +0200

    added support for direct shortcuts to command palette

--- panel/src/app/components/editor/editor.component.ts
+++ panel/src/app/components/editor/editor.component.ts
@@ -236,7 +236,7 @@ export class EditorComponent implements OnInit, OnDestroy {
 
   removeControl(control) {
     this.model.removePinLinks(control.pin);
-    this.model.control.remove(control);    
+    this.model.control.remove(control);
   }
 
   save() {
@@ -358,35 +358,40 @@ export class EditorComponent implements OnInit, OnDestroy {
     }
   }
 
+  _TesterCMDS = {
+    'e': {name: 'Edit', callback: () => this.editMode() },
+    'w': {name: 'Watch', callback: () => this.tester.watch() },
+    'c': {name: 'Console', callback: () => {
+      this.timeline.consoleActive = !this.timeline.consoleActive;
+      this.timeline.consoleExpanded = true;
+    }}
+  };
+
+  _EditorCMDS = {
+    'i': {
+      name: 'Insert',
+      direct: 'i',
+      children: {
+        'e': {name: 'Insert Expression', callback: () => this.newExpr() },
+        'v': {name: 'Insert Value', callback: () => this.newValue() },
+        's': {name: 'Insert Switch', callback: () => this.newSwitch() },
+        'c': {name: 'Insert Call', callback: () => this.newCall() },
+        'i': {name: 'New Input', callback: () => this.newInput() },
+        'o': {name: 'New Output', callback: () => this.newOutput() },
+        'l': {name: 'New Config', callback: () => this.newConfig() },
+        'k': {name: 'New Output', callback: () => this.newControl() },
+      }
+    },
+    't': {name: 'Test', callback: () => this.testMode() },
+    'w': {name: 'Watch', callback: () => this.tester.watch() }
+  };
+
   get paletteCommands() {
     if (this.tester.active) {
-      return {
-        'e': {name: 'Edit', callback: () => this.editMode() },
-        'w': {name: 'Watch', callback: () => this.tester.watch() },
-        'c': {name: 'Console', callback: () => {
-          this.timeline.consoleActive = !this.timeline.consoleActive;
-          this.timeline.consoleExpanded = true;
-        }}
-      }
+      return this._TesterCMDS;
     }
     else {
-      return {
-        'i': {
-          name: 'Insert',
-          children: {
-            'e': {name: 'Insert Expression', callback: () => this.newExpr() },
-            'v': {name: 'Insert Value', callback: () => this.newValue() },
-            's': {name: 'Insert Switch', callback: () => this.newSwitch() },
-            'c': {name: 'Insert Call', callback: () => this.newCall() },
-            'i': {name: 'New Input', callback: () => this.newInput() },
-            'o': {name: 'New Output', callback: () => this.newOutput() },
-            'l': {name: 'New Config', callback: () => this.newConfig() },
-            'k': {name: 'New Output', callback: () => this.newControl() },
-          }
-        },
-        't': {name: 'Test', callback: () => this.testMode() },
-        'w': {name: 'Watch', callback: () => this.tester.watch() }
-      }
+      return this._EditorCMDS;
     }
   }
 }
