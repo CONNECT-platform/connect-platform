ed7240029bcffd15aedf2f6f22b3f3ef96edb0bc
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2019-06-10 23:45:04 +0200

    resolved #76

--- platform/core/expression.js
+++ platform/core/expression.js
@@ -9,21 +9,36 @@ class Expression extends base.node.Node {
     super({ inputs: inputs || [], outputs: [_Result]});
 
     this._expr = expression;
-    this._script = script(this._expr);
+
+    try {
+      this._script = script(this._expr);
+    } catch(error) {
+      if (error instanceof SyntaxError)
+        this._compileError = new Error(`<p class="hl-red">${error.message}</p>
+        <p class="hl-gray">line ${error.stack.split('\n')[0].split(':')[1]}</p><br>${error.stack.split('\n')[1]}`);
+      else
+        this._compileError = error;
+    }
+
     this.pins.result = this.pins.out[_Result];
 
     //this._sync = true;
   }
 
   run(inputs, output, _, error) {
-    let context = Object.assign({console, error, require, context: this.context}, inputs);
-    let res = this._script.evaluate(context);
-    if (typeof res === 'function') {
-      if (res.length == 0) output(_Result, res());
-      else if (res.length == 1) res(val => output(_Result, val));
+    if (this._compileError) {
+      error(this._compileError);
+    }
+    else {
+      let context = Object.assign({console, error, require, context: this.context}, inputs);
+      let res = this._script.evaluate(context);
+      if (typeof res === 'function') {
+        if (res.length == 0) output(_Result, res());
+        else if (res.length == 1) res(val => output(_Result, val));
+        else output(_Result, res);
+      }
       else output(_Result, res);
     }
-    else output(_Result, res);
   }
 }
 
