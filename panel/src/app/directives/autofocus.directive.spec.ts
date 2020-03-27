86ac29333c0254f8e2a4dcd43c69655e4c23465d
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2020-03-27 15:38:53 +0100

    Fixed autofocus directive test by introducing a mock object for the ElementRef class

--- panel/src/app/directives/autofocus.directive.spec.ts
+++ panel/src/app/directives/autofocus.directive.spec.ts
@@ -1,8 +1,14 @@
 import { AutofocusDirective } from './autofocus.directive';
+import { ElementRef } from '@angular/core';
+
+class MockElementRef extends ElementRef {
+  constructor() { super(undefined); }
+  // nativeElement = {};
+}
 
 describe('AutofocusDirective', () => {
   it('should create an instance', () => {
-    const directive = new AutofocusDirective();
+    const directive = new AutofocusDirective(new MockElementRef());
     expect(directive).toBeTruthy();
   });
 });
