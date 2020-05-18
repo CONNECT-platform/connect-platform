79db09df9e8b525ce4d64e834b976c09d1d7fbd3
Mustapha Ben Chaaben <mustapha3892@gmail.com>
2020-05-18 15:32:42 +0200

    Added test utility methods to assign values to config paths and to service URLs from environment variables

--- platform/util/config.js
+++ platform/util/config.js
@@ -37,6 +37,63 @@ class Configuration {
     return key in this._params;
   }
 
+  setFromEnvVariable(path, varName, verbose = false) {
+    const value = process.env[varName];
+    if(value) {
+      if(verbose) console.log(`Found / setting from environment variable ${varName} to ${path}`);
+      const pathComponents = path.split('.');
+  
+      let ref = this._params;
+      const nComponents = pathComponents.length - 1;
+      for(let i = 0; i < pathComponents.length - 1; i++) {
+        const pathComponent = pathComponents[i];
+  
+        if( ! (pathComponent in ref) ) {
+          ref[pathComponent] = {};
+        }
+  
+        ref = ref[pathComponent];
+      }
+
+      const lastPathComponent = pathComponents[nComponents];
+      
+      ref[lastPathComponent] = value;
+    }
+  }
+  
+  setServiceURLFromEnvVariable(name, varName, verbose = false) {
+    const value = process.env[varName];
+    if(value) {
+      if(verbose) console.log(`Found / setting service URL ${name} from environment variable ${varName} = ${value}`);
+      
+      if(! ('nodes' in this._params) ) this._params.nodes = {};
+      if(! ('service' in this._params.nodes) ) this._params.nodes.service = [];
+
+      const services = this._params.nodes.service;
+  
+      let found = false;
+      let i = 0;
+      for(i = 0; i < services.length; i++) {
+        if(name === services[i].name) {
+          found = true;
+          break;
+        }
+      }
+  
+      if(! found) {
+        if(verbose) console.log(`Service ${name} was not found... Creating one with the spec`);
+        services.push({
+          name,
+          url: value
+        });
+  
+        return;
+      }
+  
+      services[i].url = value;
+    }
+  }
+
   get core() {
     return new Proxy({}, {
       get: (_, prop) => this.get(prop),
--- platform/util/test/config.js
+++ platform/util/test/config.js
@@ -74,6 +74,88 @@ describe('config', () => {
     });
   });
 
+  describe('.setFromEnvVariable()', () => {
+    it('should add value to the config.', () => {
+      let c = config({x : 2});
+      
+      const TEST_ENVIRONMENT_VARIABLE = 'TEST_ENVIRONMENT_VARIABLE';
+      const ENVIRONMENT_VARIABLE_VALUE = 'test_environment_variable';
+      process.env[TEST_ENVIRONMENT_VARIABLE] = ENVIRONMENT_VARIABLE_VALUE;
+      
+      c.setFromEnvVariable('y', TEST_ENVIRONMENT_VARIABLE);
+      
+      c.has('y').should.be.true;
+      c.get('y').should.equal(ENVIRONMENT_VARIABLE_VALUE);
+    });
+
+    it('should override the config when key already has a value.', () => {
+      let c = config({y : 'SOMETHING_ELSE'});
+      
+      const TEST_ENVIRONMENT_VARIABLE = 'TEST_ENVIRONMENT_VARIABLE';
+      const ENVIRONMENT_VARIABLE_VALUE = 'test_environment_variable';
+      process.env[TEST_ENVIRONMENT_VARIABLE] = ENVIRONMENT_VARIABLE_VALUE;
+      
+      c.setFromEnvVariable('y', TEST_ENVIRONMENT_VARIABLE);
+      
+      c.has('y').should.be.true;
+      c.get('y').should.equal(ENVIRONMENT_VARIABLE_VALUE);
+    });
+
+    it('should set value to the config for a nested path.', () => {
+      let c = config({x : { y: { z: 'SOMETHING_ELSE'} } });
+      
+      const TEST_ENVIRONMENT_VARIABLE = 'TEST_ENVIRONMENT_VARIABLE';
+      const ENVIRONMENT_VARIABLE_VALUE = 'test_environment_variable';
+      process.env[TEST_ENVIRONMENT_VARIABLE] = ENVIRONMENT_VARIABLE_VALUE;
+      
+      c.setFromEnvVariable('x.y.z', TEST_ENVIRONMENT_VARIABLE);
+      
+      c.has('x').should.be.true;
+      c.get('x').y.z.should.equal(ENVIRONMENT_VARIABLE_VALUE);
+    });
+  });
+
+  describe('.setServiceURLFromEnvVariable()', () => {
+    it('should add service.', () => {
+      let c = config({x : 2});
+      
+      const TEST_ENVIRONMENT_VARIABLE = 'TEST_ENVIRONMENT_VARIABLE';
+      const TEST_SERVICE_NAME = 'test_service';
+      const ENVIRONMENT_VARIABLE_SERVICE_URL = 'http://test_service';
+      process.env[TEST_ENVIRONMENT_VARIABLE] = ENVIRONMENT_VARIABLE_SERVICE_URL;
+      
+      c.setServiceURLFromEnvVariable(TEST_SERVICE_NAME, TEST_ENVIRONMENT_VARIABLE);
+      
+      const nodes = c.get('nodes');
+      
+      c.has('nodes').should.be.true;
+      nodes.should.have.property('service');
+      nodes.service.should.be.an('array');
+      nodes.service.should.deep.include({ name: TEST_SERVICE_NAME, url: ENVIRONMENT_VARIABLE_SERVICE_URL });
+    });
+
+    it('should override an existing service URL config.', () => {
+      const TEST_ENVIRONMENT_VARIABLE = 'TEST_ENVIRONMENT_VARIABLE';
+      const TEST_SERVICE_NAME = 'test_service';
+      const ENVIRONMENT_VARIABLE_SERVICE_URL = 'http://test_service';
+      process.env[TEST_ENVIRONMENT_VARIABLE] = ENVIRONMENT_VARIABLE_SERVICE_URL;
+      
+      let c = config({
+        y : 'SOMETHING_ELSE',
+        nodes: {
+          service: [
+            { name: TEST_SERVICE_NAME, url: 'http://random_url' }
+          ]
+        }
+      });
+
+      c.setServiceURLFromEnvVariable(TEST_SERVICE_NAME, TEST_ENVIRONMENT_VARIABLE);
+      
+      const nodes = c.get('nodes');
+      nodes.service.should.deep.include({ name: TEST_SERVICE_NAME, url: ENVIRONMENT_VARIABLE_SERVICE_URL });
+    });
+  });
+
   describe('.core', () => {
     it('should return a proxy dict handled by the config object.', () => {
       let c = config();
